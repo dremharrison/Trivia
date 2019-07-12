9e86d9db0a7eee97d776aaed6c3de58eec1392bc
dremharrison <deandremharrison@gmail.com>
2019-07-12 17:35:44 -0400

    onClick on answer buttons

--- app/src/main/java/com/example/trivia/MainActivity.java
+++ app/src/main/java/com/example/trivia/MainActivity.java
@@ -7,12 +7,14 @@ import androidx.fragment.app.FragmentTransaction;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import com.example.trivia.pojo.ResultsItem;
 import com.example.trivia.pojo.TriviaResponse;
 import com.example.trivia.retrofit.QuestionService;
 import com.example.trivia.retrofit.RetrofitClientInstance;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import retrofit2.Call;
@@ -21,6 +23,8 @@ import retrofit2.Callback;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivitys";
+    QuestionFragment questionFragment;
+    TextView question;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,12 +38,12 @@ public class MainActivity extends AppCompatActivity {
         FragmentTransaction transaction = manager.beginTransaction();
 
         //Declare and Init Fragment
-        QuestionFragment questionFragment = new QuestionFragment();
+        questionFragment = new QuestionFragment();
 
         //Add Fragment
         transaction.add(R.id.frame_layout, questionFragment, "QuestionFragment" ).commit();
 
-        retrofitRequest(20);
+        retrofitRequest(10, "multiple");
 
         findViewById(R.id.btn_start_game).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -55,22 +59,29 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void retrofitRequest(int count) {
+    public void retrofitRequest(int count, String type) {
+        final List answers = new ArrayList<String>();
+
+
         // 1. Declare GiphyService and Init using RetrofitClientInstance
         QuestionService questionService = RetrofitClientInstance.getRetrofit().create(QuestionService.class);
 
         // Declare GiphyService Return type and Init using the GiphyService from step 1
-        Call<TriviaResponse> giphycall = questionService.loadQuestion(count);
+        Call<TriviaResponse> triviaResponseCall = questionService.loadQuestion(count, type);
 
         // 3. Use the giphyCall from step 2 and call the .enqueue method
-        giphycall.enqueue(new Callback<TriviaResponse>() {
+        triviaResponseCall.enqueue(new Callback<TriviaResponse>() {
             @Override
             public void onResponse(Call<TriviaResponse> call, retrofit2.Response<TriviaResponse> response) {
 
                 if (response.isSuccessful()){
+                    ResultsItem resultsItem = response.body().getResults().get(0);
                     Log.d(TAG, "onResponse: Success" + response.body());
-                    ResultsItem resultsItem = new ResultsItem();
+                    String correctAnswer = resultsItem.getCorrectAnswer();
+                    answers.addAll(resultsItem.getIncorrectAnswers());
+                    answers.add(correctAnswer);
                     String question = resultsItem.getQuestion();
+                    sendQuestionAndAnswers(question, correctAnswer, answers);
 
                 } else {
                     Log.d(TAG, "onResponse: Fail");
@@ -84,4 +95,9 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+    public void sendQuestionAndAnswers(String question, String correctAnswer, List<String> answers) {
+        questionFragment.displayQuestionAndAnswers(question, correctAnswer, answers);
+    }
 }
+
--- app/src/main/java/com/example/trivia/QuestionFragment.java
+++ app/src/main/java/com/example/trivia/QuestionFragment.java
@@ -2,16 +2,35 @@ package com.example.trivia;
 
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 
 public class QuestionFragment extends Fragment {
+    TextView tvQuestion, tvAnswer1, tvAnswer2, tvAnswer3, tvAnswer4, tvScore;
+    String checkCorrectAnswer;
+    private static final String TAG = "QuestionFragments";
+    Integer score = 0;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_question, container, false);
+        final View view = inflater.inflate(R.layout.fragment_question, container, false);
+
+        tvQuestion = view.findViewById(R.id.tv_question);
+        tvAnswer1 = view.findViewById(R.id.tv_answer_a);
+        tvAnswer2 = view.findViewById(R.id.tv_answer_b);
+        tvAnswer3 = view.findViewById(R.id.tv_answer_c);
+        tvAnswer4 = view.findViewById(R.id.tv_answer_d);
+        tvScore = view.findViewById(R.id.tv_score);
 
         view.findViewById(R.id.btn_quit).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,6 +42,81 @@ public class QuestionFragment extends Fragment {
                 v.findViewById(R.id.spinner_difficulty).setVisibility(View.VISIBLE);
             }
         });
+
+        view.findViewById(R.id.btn_a).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String btnText = (String) tvAnswer1.getText();
+                if (btnText == checkCorrectAnswer){
+                    view.findViewById(R.id.tv_right).setVisibility(View.VISIBLE);
+                    tvScore.setText(String.valueOf(score + 100));
+                } else {
+                    view.findViewById(R.id.tv_wrong).setVisibility(View.VISIBLE);
+                    tvScore.setText(String.valueOf(score - 100));
+                }
+
+            }
+        });
+
+        view.findViewById(R.id.btn_b).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String btnText = (String) tvAnswer2.getText();
+                if (btnText == checkCorrectAnswer){
+                    view.findViewById(R.id.tv_right).setVisibility(View.VISIBLE);
+                    tvScore.setText(String.valueOf(score + 100));
+                } else {
+                    view.findViewById(R.id.tv_wrong).setVisibility(View.VISIBLE);
+                    tvScore.setText(String.valueOf(score - 100));
+                }
+
+
+            }
+        });
+
+        view.findViewById(R.id.btn_c).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String btnText = (String) tvAnswer3.getText();
+                if (btnText == checkCorrectAnswer){
+                    view.findViewById(R.id.tv_right).setVisibility(View.VISIBLE);
+                    tvScore.setText(String.valueOf(score + 100));
+                } else {
+                    view.findViewById(R.id.tv_wrong).setVisibility(View.VISIBLE);
+                    tvScore.setText(String.valueOf(score - 100));
+                }
+
+            }
+        });
+
+        view.findViewById(R.id.btn_d).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String btnText = (String)tvAnswer4.getText();
+                if (btnText == checkCorrectAnswer){
+                    view.findViewById(R.id.tv_right).setVisibility(View.VISIBLE);
+                    tvScore.setText(String.valueOf(score + 100));
+                } else {
+                    view.findViewById(R.id.tv_wrong).setVisibility(View.VISIBLE);
+                    tvScore.setText(String.valueOf(score - 100));
+                }
+
+            }
+        });
         return view;
+
+
+    }
+
+    public void displayQuestionAndAnswers(String question, String correctAnswer, List<String> answers) {
+        tvQuestion.setText(question);
+        checkCorrectAnswer = correctAnswer;
+        Log.d(TAG, "displayQuestionAndAnswers: " + correctAnswer);
+        Collections.shuffle(answers);
+        tvAnswer1.setText(answers.get(0));
+        tvAnswer2.setText((answers.get(1)));
+        tvAnswer3.setText(answers.get(2));
+        tvAnswer4.setText(answers.get(3));
+
     }
 }
